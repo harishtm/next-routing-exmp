4ad0e5bacb4c7c6d875969df49e934a5e2f4d3c2
Harish T M <harishtm77@gmail.com>
2024-09-12 16:56:41 +0530

    public route with underscore

---
+++ src/app/%5Fphoto/page.tsx
@@ -0,0 +1,11 @@
+const Photo = () => {
+    return (
+        <div>
+            <h2>
+                URL route with underscore (_) specify the folder name with (%5F)
+            </h2>
+        </div>
+    )
+}
+
+export default Photo;
