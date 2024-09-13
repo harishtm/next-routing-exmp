2f28832997054f232a6bd90d83a559c7d934dc30
Harish T M <harishtm77@gmail.com>
2024-09-13 12:44:36 +0530

    Nested Layouts and Routing Meta Data

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,7 @@
+export const metadata = {
+    title: "About Us Page"
+}
+
 export default function AboutPage () {
     return <h2>About page</h2>
 }
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,8 +1,24 @@
-const ProductDetail = ({
-    params
-}: {
-    params: {productId: string};
-}) => {
+import { Metadata } from "next";
+
+type ProductDetailProps = {
+    params: {
+        productId: string
+    }
+}
+
+export const generateMetadata = async ({ params}: ProductDetailProps): Promise<Metadata> => {
+    const title = await new Promise(resolve => {
+        setTimeout(() => {
+            resolve(`iPhone ${params.productId}`)
+        }, 1000)
+    });
+
+    return {
+        title: `Product ${title}`
+    }
+}
+
+const ProductDetail = ({ params }: ProductDetailProps) => {
     return (
         <h2>Product Detail {params.productId}</h2>
     )
